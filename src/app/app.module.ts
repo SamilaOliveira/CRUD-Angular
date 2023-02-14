d4a6f80dfe18f7998f5dc682ed85d961208f3cf3
SamilaOliveiragit config --global user.email samilacampelo@gmail.comgit config --global user.name SamilaOliveira <samilacampelo@gmail.com>
2023-02-14 17:22:11 -0300

    Construção da View do projeto. Event Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarFormularioComponent } from './componentes/formulario/criar-formulario/criar-formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarFormularioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/formulario/criar-formulario/criar-formulario.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-formulario',
+  templateUrl: './criar-formulario.component.html',
+  styleUrls: ['./criar-formulario.component.css']
+})
+export class CriarFormularioComponent {
+
+  formulario = {
+    id: '1',
+    nome:  'Samila Oliveira',
+    conteudo: 'Projeto Angular',
+    modelo: ''
+  }
+
+  constructor() {}
+
+}
