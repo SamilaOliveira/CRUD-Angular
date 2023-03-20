90743903677a4904b47169bbeed0a4c0228a1893
SamilaOliveiragit config --global user.email samilacampelo@gmail.comgit config --global user.name SamilaOliveira <samilacampelo@gmail.com>
2023-03-20 17:14:45 -0300

    Editar formulario concluído.

--- src/app/componentes/formulario/editar-formulario/editar-formulario.component.ts
+++ src/app/componentes/formulario/editar-formulario/editar-formulario.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Formulario } from '../formulario';
+import { FormularioService } from '../formulario.service';
 
 @Component({
   selector: 'app-editar-formulario',
   templateUrl: './editar-formulario.component.html',
   styleUrls: ['./editar-formulario.component.css']
 })
-export class EditarFormularioComponent {
+export class EditarFormularioComponent implements OnInit {
 
+  formulario: Formulario = {
+    id: 0,
+    nome: '',
+    email: '',
+    modelo:''
+  }
+
+  constructor(
+      private service: FormularioService,
+      private router: Router,
+      private route: ActivatedRoute
+  ){ }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((formulario) =>
+      this.formulario = formulario
+  )}
+
+  editarCadastro(){
+      this.service.editar(this.formulario).subscribe(() =>
+          this.router.navigate(['/listarFormulario'])
+  )}
+
+  cancelar(){
+      this.router.navigate(['/listarFormulario'])
+  }
 }
